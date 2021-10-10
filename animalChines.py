a9d2df3f0f8a173191a773a809c64844b11cff07
albertoribeiro <alberto.r.flavio@gmail.com>
2021-10-09 23:08:56 -0300

    fixes

--- animalChines.py
+++ animalChines.py
@@ -5,32 +5,32 @@ from ask_sdk_core.utils import is_request_type, is_intent_name
 
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
-        print("> LaunchRequestHandler__can_handle")
+        print("> LaunchRequestHandler-can_handle")
         return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        print("> LaunchRequestHandler")
+        print("> LaunchRequestHandler-handle")
         handler_input.response_builder.speak("Bem vindo à skill do animal chinês").set_should_end_session(False)
         return handler_input.response_builder.response
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
     def can_handle(self, handler_input, exception):
-        print("> CatchAllExceptionHandler__can_hanlde")
+        print("> CatchAllExceptionHandler-can_handle")
         return True
 
     def handle(self, handler_input, exception):
-        print("> CatchAllExceptionHandler")
-        print(exception)
+        print("> CatchAllExceptionHandler-handle")
         handler_input.response_builder.speak("Desculpe, ocorreu um problema. Por favor tente outra vez!! ").set_should_end_session(False)
         return handler_input.response_builder.response 
 
 class AnimalChinesIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
-        print("> AnimalChinesIntentHandler__can_handle")
+        print("> AnimalChinesIntentHandler-can_handle")
         return is_intent_name("AnimalChinesIntent")(handler_input)
 
     def handle(self, handler_input, exception):
-        print("> AnimalChinesIntentHandler")
+        print("> AnimalChinesIntentHandler-handle")
+        print(exception)
         ano = handler_input.request_envelope.request.intent.slots['ano'].value
         speech_text = "o ano que vc disse foi "
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
@@ -42,5 +42,5 @@ sb.add_exception_handler(CatchAllExceptionHandler())
 sb.add_request_handler(AnimalChinesIntentHandler())
 
 def handler(event, context):
-    print("> Hello")
+    print("> handler")
     return sb.lambda_handler()(event, context)
