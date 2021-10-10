e5e7a1fe8af2c4267c205a4f5f1e1f983f8c6882
albertoribeiro <alberto.r.flavio@gmail.com>
2021-10-09 22:04:15 -0300

    fixes

--- animalChines.py
+++ animalChines.py
@@ -5,26 +5,32 @@ from ask_sdk_core.utils import is_request_type, is_intent_name
 
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
+        print("> LaunchRequestHandler__can_handle")
         return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
+        print("> LaunchRequestHandler")
         handler_input.response_builder.speak("Bem vindo à skill do animal chinês").set_should_end_session(False)
         return handler_input.response_builder.response
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
-    def can_hanlde(self, handler_input, exception):
+    def can_handle(self, handler_input, exception):
+        print("> CatchAllExceptionHandler__can_hanlde")
         return True
 
     def handle(self, handler_input, exception):
+        print("> CatchAllExceptionHandler")
         print(exception)
         handler_input.response_builder.speak("Desculpe, ocorreu um problema. Por favor tente outra vez!! ").set_should_end_session(False)
         return handler_input.response_builder.response 
 
 class AnimalChinesIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
+        print("> AnimalChinesIntentHandler__can_handle")
         return is_intent_name("AnimalChinesIntent")(handler_input)
 
     def handle(self, handler_input, exception):
+        print("> AnimalChinesIntentHandler")
         ano = handler_input.request_envelope.request.intent.slots['ano'].value
         speech_text = "o ano que vc disse foi "
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
@@ -36,5 +42,5 @@ sb.add_exception_handler(CatchAllExceptionHandler())
 sb.add_request_handler(AnimalChinesIntentHandler())
 
 def handler(event, context):
-    print("Hello World!")
+    print("> Hello")
     return sb.lambda_handler()(event, context)
